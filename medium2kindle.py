9794ceea656b65a565890ba1edd29abe06443ba1
Karol Bieńkowski <karolbienkowski@protonmail.com>
2020-03-27 18:25:36 +0100

    code overhaul, shorter, faster, less buggy

--- medium2kindle.py
+++ medium2kindle.py
@@ -2,36 +2,36 @@
 # author: Karol Bieńkowski
 # license: GPL v3
 
-from urllib.request import urlopen, Request
-import sys, subprocess, os, io
+
+import sys, subprocess, os, io, requests
 from bs4 import BeautifulSoup
 from PIL import Image
 
 hotmail_pass = 'password'
-hotmail_adress = 'example@hotmail.com' # which script also assumes is your login, too
+hotmail_adress = 'example@hotmail.com' # script assumes it is your login, too
 kindle_adress = 'example@kindle.com'
 
 def convert ( file_name, author ):
-	print("\nFile conversion started...")
+	print('\nFile conversion started...')
 	
-	# ebook-convert file_name+".html" file_name+".mobi" --output-profile kindle --mobi-file-type=both --enable-heuristics --authors authors
-	subprocess.run(['ebook-convert', file_name+".html", file_name+".mobi", "--no-inline-toc", "--pretty-print", "--output-profile", "kindle", "--mobi-file-type=both", "--authors", author], stdout=subprocess.DEVNULL)
+	# ebook-convert file_name+'.html' file_name+'.mobi' --output-profile kindle --mobi-file-type=both --enable-heuristics --authors authors
+	subprocess.run(['ebook-convert', file_name+'.html', file_name+'.mobi', '--no-inline-toc', '--pretty-print', '--output-profile', 'kindle', '--mobi-file-type=both', '--authors', author], stdout=subprocess.DEVNULL)
 
-	print("Conversion successful.")
+	print('Conversion successful.')
 
 
 def send (file_name):
-	print("\nSending file to Kindle device...")
+	print('\nSending file to Kindle device...')
 	# you can add --fork but then photos will do weird shit. do sth to fix that.
 	calibre_smtp = ' --relay smtp.live.com --port 587 --username ' + hotmail_adress + ' --password ' + hotmail_pass + ' --encryption-method TLS ' + hotmail_adress + ' ' + kindle_adress + ' ""'
-	subprocess.call(['calibre-smtp', '-a', file_name+".mobi"] + calibre_smtp.split(), stdout=subprocess.DEVNULL)
+	subprocess.call(['calibre-smtp', '-a', file_name+'.mobi'] + calibre_smtp.split(), stdout=subprocess.DEVNULL)
 	
-	print("File sent.")
+	print('File sent.')
 
 
 def clean (file_name, n):	
 	try:
-		for ext in [".html",".mobi"]:	#".html",
+		for ext in ['.html','.mobi']:	#'.html',
 			os.remove(file_name+ext)
 	except:
 		pass
@@ -48,40 +48,46 @@ def clean (file_name, n):
 
 
 def draw_line ():
-	print('\n' + '_' * 40)
+	print('\n' + '_' * 40 + '\n')
 
 
 def find_author (page):
 	try:
-		x = page.find('meta', {'name': 'author'})['content']
+		return page.find('meta', {'name': 'author'})['content']
 	except:
-		x = 'unknown'
-	
-	return x
+		print('Som Ding Wong. Could not find author name in requested article page.')
+		return 'unknown'
 
 
 def find_title (page):
-	# TODO: find proper way to get only article title
-	title_tag = page.find('meta', {'property': 'twitter:title'})
-	
-	if title_tag is None:
-		print('\nNo title tag. Trying to extract from article.')
-		return page.title_tag.get_text().split('–')[0].split('-')[0]
-	else:
-		return title_tag['content']
-	
+	# It just looks for multiple sources for title in site
+	try:
+		return page.find('meta', {'property': 'og:title'})['content']
+	except:
+		try:
+			return page.find('meta', {'property': 'twitter:title'})['content']		
+		except:
+			try:
+				return page.find('meta', {'name': 'title'})['content'].split('|')[0]
+			except:
+				try:
+					return page.head.title.get_text().split('|')[0]
+				except:
+					print('Som Ding Wong. Could not find any title in requested article page.')
+					return 'unknown'
+				
 
 def sanitize_images (section, n):
 	for image in section.select('img[src]'):
-		n += 1
-		resource = urlopen(Request(image['src'], None, header), timeout=10)
-		#print(image['src'])
-		
-		if image['src'].split('?')[-1] == 'q=20':
+		if image['src'].split('?')[-1] == 'q=20' or int(image['width']) <= 100:
 			image.decompose()
 			continue
 		
-		img = Image.open(io.BytesIO(resource.read()))
+		n += 1
+		#resource = urlopen(Request(image['src'], None, header), timeout=10)
+		resource = requests.get(image['src'], headers=header).content
+		
+		img = Image.open(io.BytesIO(resource))
 		width, height = img.size
 		
 		if width >= 1600 or height >= 1600:
@@ -104,61 +110,36 @@ article_links = sys.argv
 
 for article in range(1,len(article_links)):
 	url = article_links[article]
-	header = {'User-Agent': 'Mozilla/5.0 (X11; U; Linux i686) Gecko/20071127 Firefox/2.0.0.11'}
-	
-	page_req = Request(url, None, header)
-	html = urlopen(page_req, timeout=10).read().decode('utf-8', 'ignore')
-	page = BeautifulSoup(html, "lxml")
+	header = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36'}	
+	html = requests.get(url, headers=header).text
+	page = BeautifulSoup(html, 'lxml')
 	
 	draw_line()
 	
 	author = find_author(page)
 	title = find_title (page)
 	file_name = title
+	print('title: ', file_name, '\nauthor: ', author)
+	
+	sections = page.body.div.div.article
+	
+	for svg in sections.find_all("svg"):
+		svg.decompose()
+	
+	for h1 in sections.find_all("h1"):
+		h1.decompose()
+	'''
+	orig_file = open(file_name+'_orig.html', 'w')
+	orig_file.write(page.prettify(formatter='html'))
+	orig_file.close()
+	'''
+	#print('number of sections: ', len(sections))
 
-	print("\ntitle: ", file_name, "\nauthor: ", author)
-	
-	old_design = page.find('div', {'class': 'section-content'}) is not None
-	
-	if old_design:
-		print ("oldest design")
-		
-		for match in page.find_all('div', {'class': 'aspectRatioPlaceholder-fill'}):
-			match.decompose()
-			
-		for match in page.find_all('h1', {'class': 'graf'}):
-			match.decompose()
-		
-		for match in page.find_all('img', {'class': 'graf-dropCapImage'}):
-			match.decompose()
-		
-		page.find('div', {'class': 'uiScale'}).decompose()
-		
-		sections = page.find_all('div', {'class': 'section-content'})
-		
-	else:
-		sections = page.body.div.div.article
-		#I may try this:
-		#sections.div.section.contents[1].div.decompose()
-		#print ("new design")
-		#try:
-			#page.find('h1').decompose()
-		#except:
-			#pass	
-		#page.find('svg').parent.parent.parent.parent.decompose()
-		
-		#you prolly need only the bigger one
-		#sections = page.find_all('section')
-		#if len(sections) > 3:
-			#sections = sections[2:-1]
-		#else:
-			#sections = sections[2:4]
-	
 	g = open(file_name+'.html', 'w')
-	g.write('<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN"><html lang="en"><head><title>' + title + '</title><meta http-equiv="content-type" content="text/html; charset=utf-8"/></head><body><div>')
+	g.write('<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN"><html lang="en"><head><title>' + title + '</title><meta http-equiv="content-type" content="text/html; charset=utf-8"/></head><body><div>\n')
 	
 	n = 0
-	article_len = 0 
+	article_len = 0
 	for section in sections:
 		[section, n] = sanitize_images(section, n)
 		article_len += len(section.get_text())
@@ -173,7 +154,6 @@ for article in range(1,len(article_links)):
 	g.write('</div></body></html>')
 	g.close()
 	
-
 	if article_len < 1050:
 		print("Article is too short, probably partial, not sending.")
 		clean (file_name, n)
